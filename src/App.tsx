ae775df6a1b813aa932d79f15898d89f2aa7be86
dbstj0403 <wcn09088@naver.com>
2023-11-04 22:45:44 +0900

    #6 Todo Setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import { createGlobalStyle } from "styled-components";
+import ToDoList from "./TodoList";
 const GlobalStyle = createGlobalStyle`
   @import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400&display=swap');
 	
@@ -9,6 +10,7 @@ function App() {
   return (
     <>
       <GlobalStyle />
+      <ToDoList />
     </>
   );
 }
