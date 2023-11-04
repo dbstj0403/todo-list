bd0ced071375b40cd4f9a4fda290c54b4f30c87a
dbstj0403 <wcn09088@naver.com>
2023-11-04 22:33:35 +0900

    #6 Setup

---
+++ src/theme.ts
@@ -0,0 +1,21 @@
+import { DefaultTheme } from "styled-components";
+
+export const theme: DefaultTheme = {
+  bgColor: "#2f3640",
+  textColor: "white",
+  accentColor: "pink",
+};
+
+export const darkTheme: DefaultTheme = {
+  bgColor: "#2f3640",
+  textColor: "white",
+  accentColor: "#9c88ff",
+  cardBgColor: "transparent",
+};
+
+export const lightTheme: DefaultTheme = {
+  bgColor: "whitesmoke",
+  textColor: "black",
+  accentColor: "#9c88ff",
+  cardBgColor: "white",
+};
